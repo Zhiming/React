87c4898ab3501cfe25a0a65816b6a37bcca42f12
Zhiming Zhang <athrun07091988@gmail.com>
2019-03-08 20:21:30 -0800

    引入了immutable.js并且完成了搜索框的基本布局

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,43 +1,86 @@
-import React from 'react';
-import {HeaderWrapper, Logo, Nav, NavItem, SearchWrapper, NavSearch, Addition, Button} from './style';
+import React, {Component} from 'react';
+import {
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    SearchInfo,
+    SearchInfoTitle,
+    SearchWrapper,
+    SearchInfoSwitch,
+    SearchInfoItem,
+    SearchInfoList,
+    NavSearch,
+    Addition,
+    Button
+} from './style';
 import { CSSTransition } from 'react-transition-group';
 import {connect} from 'react-redux';
 import { actionCreators } from './store/';
 
-const Header = (props) => {
-    return (
-        <HeaderWrapper>
-            <Logo/>
-            <Nav>
-                <NavItem className='left active'>首页</NavItem>
-                <NavItem className='left'>下载APP</NavItem>
-                <NavItem className='right'>登录</NavItem>
-                <NavItem className='right'>Aa</NavItem>
-                <SearchWrapper>
-                    <CSSTransition
-                        in = {props.focused}
-                        timeout = {200}
-                        classNames = "slide"
-                    >
-                        <NavSearch
-                            className={props.focused ? 'focused' : ""}
-                            onFocus = {props.handleInputFocus}
-                            onBlur = {props.handleInputBlur}
-                        />
-                    </CSSTransition>
-                </SearchWrapper>
-                <Addition>
-                    <Button className="writing">写文章</Button>
-                    <Button className="reg">注册</Button>
-                </Addition>
-            </Nav>
-        </HeaderWrapper>
-    );
+class Header extends Component {
+
+    const
+
+    getListArea(show) {
+        if (show) {
+            return (
+                <SearchInfo>
+                    <SearchInfoTitle>
+                        热门搜索
+                        <SearchInfoSwitch>换一批</SearchInfoSwitch>
+                    </SearchInfoTitle>
+                    <SearchInfoList>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                        <SearchInfoItem>教育</SearchInfoItem>
+                    </SearchInfoList>
+                </SearchInfo>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    render() {
+        return (
+            <HeaderWrapper>
+                <Logo/>
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载APP</NavItem>
+                    <NavItem className='right'>登录</NavItem>
+                    <NavItem className='right'>Aa</NavItem>
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={this.props.focused}
+                            timeout={200}
+                            classNames="slide"
+                        >
+                            <NavSearch
+                                className={this.props.focused ? 'focused' : ""}
+                                onFocus={this.props.handleInputFocus}
+                                onBlur={this.props.handleInputBlur}
+                            />
+                        </CSSTransition>
+                        {this.getListArea(this.props.focused)}
+                    </SearchWrapper>
+                    <Addition>
+                        <Button className="writing">写文章</Button>
+                        <Button className="reg">注册</Button>
+                    </Addition>
+                </Nav>
+            </HeaderWrapper>
+        );
+    }
 };
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.focused
+        focused: state.getIn(["header", "focused"])
     };
 };
 
